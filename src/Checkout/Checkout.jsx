f127154f00d2cac6b213d0cbe374273a1d7c22e4
HungDuong2909 <hungduongg2909@gmail.com>
2025-07-10 15:38:55 +0700

    transports websocket

--- src/Checkout/Checkout.jsx
+++ src/Checkout/Checkout.jsx
@@ -8,6 +8,7 @@ import "./Checkout.css";
 import io from "socket.io-client";
 const socket = io(process.env.REACT_APP_SOCKET_URL, {
    withCredentials: true,
+   transports: ["websocket"],
 });
 
 function Checkout(props) {
